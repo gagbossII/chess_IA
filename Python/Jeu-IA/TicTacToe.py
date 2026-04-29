9632cff235a3ef5f7621628b9b57cfc8636ded57
Gaëtan Poli <gaetan.poli@edu.gbjb.ch>
2026-04-29 09:52:48 +0200

    Version finale de l'IA de morpion. Le jeu en lui-même n'est pas complet mais l'IA est complète.

--- Python/Jeu-IA/TicTacToe.py
+++ Python/Jeu-IA/TicTacToe.py
@@ -4,6 +4,7 @@ table = [" " for i in range(9)]
 player1 = "X"
 aiPlayer = "O"
 bestMove = 0
+bestscore = -10
 
 def writeTable() : 
     for i in range(0, 9, 3) : 
@@ -11,6 +12,7 @@ def writeTable() :
         print(f"{table[i]} | {table[i+1]} | {table[i+2]}")
         if i < 6 :
             print("---------")
+    print("\n")
 
 def writeInTable(player, index) : 
     if table[index] != " " : return
@@ -34,40 +36,60 @@ def play(turn) :
     return int(input("Joueur 1, donnez le numéro de la case où vous voulez jouer : "))
 
 def game() : 
+    global bestscore
     writeTable()
     for i in range(12) : # Dans le cas où un joueur fait une erreur et doit rejouer une fois de plus
-        if i % 2 == 0 : # Permet de faire jouer l'un après l'autre les joueurs 
+        if availablesMoves() == [] : print("Egalité")
+        if i % 2 == 0 : # Permet de faire jouer l'un après l'autre les joueurs. Tour du joueur.
             writeInTable(player1, play(i))
-        else :
-            writeInTable(aiPlayer)
-        if getWinner() == None :
+        else : # Tour de l'IA
+            writeInTable(aiPlayer, minimax(0, aiPlayer, availablesMoves()[0]))
+            bestscore = -10
+        if getWinner() == None : # Si il n'y a pas de vainqueur
             continue
         else : 
             print(f"Le gagnant est {getWinner()}")
             return
 
-def minimax(turn, player, bestScore, move) :
+def minimax(turn, player, move) :
+    global bestscore
+    global bestMove
     # Défini les scores minimaux et maximaux pour le tour. On fait ça car plus il y faut de tour pour gagner, moins le coup est bon.
     scoreMin = -1 + turn/8 
     scoreMax = 1 - turn/8 
     # Donne les scores en fonction d'une victoire, une défaite ou d'un nul et retourne le coup qui a permis d'arriver à ce résultat
+    if (turn == 2 and getWinner() == player1 and bestscore < 1) :
+        bestscore = 1
+        return move
+    if (turn == 1 and getWinner() == aiPlayer) :
+        bestscore = 2
+        return move
     if (getWinner() == player1) :
-        if bestScore < scoreMin : 
-            bestScore = scoreMin
+        if bestscore < scoreMin : 
+            bestscore = scoreMin
             return move
     if (getWinner() == aiPlayer) :
-        if bestScore < scoreMax : 
-            bestScore = scoreMax
+        if bestscore < scoreMax : 
+            bestscore = scoreMax
             return move
     if (" " not in table) :
-        if bestScore < 0 :
-            bestScore = 0 
+        if bestscore < 0 :
+            bestscore = 0 
             return move
     # Mise en place de la récursivité pour permettre à l'algorithme de voir tout les coups possibles 
     
     if player == aiPlayer :
         for moves in availablesMoves() :
-            table[moves] = player1
-            bestMove = minimax(turn+1, aiPlayer, bestScore, moves)
+            table[moves] = aiPlayer
+            bestMove = minimax(turn+1, player1, moves)
             table[moves] = " "
+        return bestMove
+    else :
+        for moves in availablesMoves() :
+            table[moves] = player1
+            bestMove = minimax(turn+1, aiPlayer, moves)
+            table[moves] = " "
+        return bestMove
+
+game()
         
